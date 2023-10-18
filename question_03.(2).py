04a99ea0a03431ca25b41e2cff00f74b4d27a7d3
riddhi2309 <141670096+riddhi2309@users.noreply.github.com>
2023-10-18 09:50:54 +0530

    question_03.(2).py

--- question_03.(2).py
+++ question_03.(2).py
@@ -1,12 +1,21 @@
-# a ='HELLO PYTHON'
-# a1='Pyhton Hello'
-# a2 = 'olleH nohtyP'
-# a =a[ 12: 0:-1]
-# print(a)
-# a =a.split()
-# a=list(a)
-# print(a[6:],a[:6])
-# a=a.replace(" ", "_")
-# a=a.lower()
-# print(a)
+n = float(input("Enter the number:-"))
+error = float("Enter the error:-")
+i = 1
+fact=1
+f_i=n
+f_i1 = f_i - n**(2*i+1)
+f_i1=n-(n**3)/6 
+
+while abs(f_i1-f_i)>=error:
+    print(f"Iter{i}:",abs(f_i1-f_i))
+    f_i=f_i1
+    i+=1
+    fact=fact*((2*i+1)*(2*i))
+    if i%2==0:
+        f_i1 = f_i1 + (n**(2*i+1))/fact
+    else:
+        f_i1 = f_i1 - (n**(2*i+1))/fact
+
+print("no. of terms: ",i+1)
+print("f_i+1: ",f_i1)
 
